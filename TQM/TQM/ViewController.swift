dbf9ea688fd12c204b5dcecfb7de049ba20ecdfa
Diego De Dios <diegodedios@Diegos-Mac-mini.local>
2023-10-14 14:46:54 -0400

    branch

--- TQM/TQM/ViewController.swift
+++ TQM/TQM/ViewController.swift
@@ -12,5 +12,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        let font = UIFont(name: "Arial", size: 14);
+        let normalTextAttributes: [NSAttributedString.Key: Any] = [
+            .font: font
+        ];
+        
+        tabs.setTitleTextAttributes(normalTextAttributes, for: .normal);
     }
+    
+    @IBOutlet var tabs: UISegmentedControl!;
 }
