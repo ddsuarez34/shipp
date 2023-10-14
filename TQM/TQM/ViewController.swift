64be7984509d297f0878d0c213ac09aac206e721
Diego De Dios <diegodedios@Diegos-Mac-mini.local>
2023-10-14 16:13:21 -0400

    buttons

--- TQM/TQM/ViewController.swift
+++ TQM/TQM/ViewController.swift
@@ -14,4 +14,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
 
     }
+    @IBAction func onSignUpTabClick(_ sender: Any) {
+        signInStack.isHidden = true;
+        signUpStack.isHidden = false;
+
+    }
+    @IBAction func onSignInTabClick(_ sender: Any) {
+        signInStack.isHidden = false;
+        signUpStack.isHidden = true;
+    }
+    
+    @IBOutlet var signUpStack: UIStackView!
+    @IBOutlet var signInStack: UIStackView!
 }
